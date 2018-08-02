ca5194fc59c9976342903c6d0f2d7801a16619d8
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2018-08-02 21:07:16 +0200

    Moved from gulp to mix

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -57,7 +57,7 @@
             <link href="{{ asset('css/flatly.css') }}" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-            <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+            <link href="{{ mix('css/front.css') }}" rel="stylesheet">
         @show
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -96,9 +96,8 @@
         @show
         @yield('content')
 
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/lightbox.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="{{ asset('js/grids.min.js') }}"></script>
         <script src="{{ asset('js/roelof.js') }}"></script>
     </body>
