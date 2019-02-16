9a004a1efc60038552335367d7122d74681a4692
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2019-02-16 20:00:13 +0100

    Removed fontawesome

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -26,6 +26,8 @@
             <title>{{ $page->title }}</title>
 		@show
 
+        <link rel="dns-prefetch" href="https://www.google-analytics.com">
+
         @if(\Main\Classes\Canonical::needsLink())
             <link rel="canonical" href="{{\Main\Classes\Canonical::getLink()}}" />
         @endif
@@ -59,7 +61,6 @@
 
         <!-- Bootstrap -->
         @section('stylesheets')
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link href="{{ mix('css/front.css') }}" rel="stylesheet">
         @show
 
