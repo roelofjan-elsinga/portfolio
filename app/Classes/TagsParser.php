e6ad090b71daad4ce74d70a080f9db0f1fd9db75
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2019-03-12 18:19:54 +0100

    Added default meta tags

--- app/Classes/TagsParser.php
+++ app/Classes/TagsParser.php
@@ -11,21 +11,70 @@ class TagsParser
         return new TagsParser();
     }
 
+    /**
+     * Get the meta tags for the given page name
+     *
+     * @param string $name
+     * @return \stdClass
+     */
     public function getTagsForPageName(string $name): \stdClass
     {
         $all_tags = $this->getAllTags();
+
+        $default_tags = $this->getDefaultTags();
 
         $filtered_array = array_filter($all_tags, function ($tags, $pageName) use ($name) {
             return $pageName === $name;
         }, 1);
 
         if (isset($filtered_array[$name])) {
-            return $filtered_array[$name];
+            return $this->fillInBlankFieldsWithDefault($filtered_array[$name], $default_tags);
         } else {
-            return head($all_tags);
+            return $this->fillInBlankFieldsWithDefault(head($all_tags), $default_tags);
         }
     }
 
+    /**
+     * Get the default tags for any missing information
+     *
+     * @return \stdClass
+     */
+    private function getDefaultTags(): \stdClass
+    {
+        $parsed_config = $this->parseConfigFile();
+
+        $default_tags = collect($parsed_config['tags'])
+            ->filter(function ($tags, $pageName) {
+                return $pageName === 'default';
+            })
+            ->first();
+
+        return $this->convertArrayToStdClass($default_tags);
+    }
+
+    /**
+     * Fill in any missing fields with the default values, if they exist
+     *
+     * @param $tags
+     * @param $default_tags
+     * @return \stdClass
+     */
+    private function fillInBlankFieldsWithDefault($tags, $default_tags): \stdClass
+    {
+        foreach ($tags as $key => $value) {
+            if (is_null($value) && isset($default_tags->$key)) {
+                $tags->$key = $default_tags->$key;
+            }
+        }
+
+        return $tags;
+    }
+
+    /**
+     * Get all meta tags in the config file
+     *
+     * @return array
+     */
     public function getAllTags(): array
     {
         $parsed_config = $this->parseConfigFile();
@@ -37,6 +86,12 @@ class TagsParser
             ->toArray();
     }
 
+    /**
+     * Convert an array to a stdClass
+     *
+     * @param array $input_array
+     * @return \stdClass
+     */
     private function convertArrayToStdClass(array $input_array): \stdClass
     {
         $output_class = new \stdClass();
@@ -48,6 +103,11 @@ class TagsParser
         return $output_class;
     }
 
+    /**
+     * Parse the meta tags config file
+     *
+     * @return array
+     */
     public function parseConfigFile(): array
     {
         return Yaml::parseFile(
