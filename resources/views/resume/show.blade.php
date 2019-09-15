466094c2a310e7e722abfc138382440cc6ccd5b5
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2019-09-15 15:23:17 +0200

    Added meta tag for responsive resume

--- resources/views/resume/show.blade.php
+++ resources/views/resume/show.blade.php
@@ -4,6 +4,7 @@
         CV Roelof Jan Elsinga
     </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{mix('css/resume.css')}}" rel="stylesheet"/>
 </head>
 
