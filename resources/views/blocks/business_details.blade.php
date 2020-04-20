906fce1327bea05b10aba1d5a14f73b9c2fc52e1
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2020-04-20 12:33:17 +0200

    Added BTW # to footer

--- resources/views/blocks/business_details.blade.php
+++ resources/views/blocks/business_details.blade.php
@@ -1,13 +1,17 @@
-<div class="flex justify-center align-center text-center items paragraph-spacing my-8 text-lg text-theme-darkest rounded p-4">
-    <div class="flex-1">
+<div class="flex flex-col md:flex-row justify-center align-center text-center items paragraph-spacing my-8 text-lg text-theme-darkest rounded p-4">
+    <div class="flex-1 mb-2 md:mb-0">
         &copy; Roelof Jan Elsinga
     </div>
 
-    <div class="flex-1">
+    <div class="flex-1 mb-2 md:mb-0">
         KvK: 64377962
     </div>
 
-    <a href="{{route('page', 'algemene-voorwaarden')}}" class="flex-1 text-theme-darkest">
+    <div class="flex-1 mb-2 md:mb-0">
+        BTW: NL003051282B11
+    </div>
+
+    <a href="{{route('page', 'algemene-voorwaarden')}}" class="flex-1 text-theme-darkest underline">
         Algemene voorwaarden
     </a>
 </div>
