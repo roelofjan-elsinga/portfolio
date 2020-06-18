1026874cdd25b4d38526cdba20aa05cff30adf82
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2020-06-18 13:19:07 +0200

    Commented Google Analytics

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -163,23 +163,13 @@
         </script>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-49160339-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+{{--        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-49160339-1"></script>--}}
+{{--        <script>--}}
+{{--            window.dataLayer = window.dataLayer || [];--}}
+{{--            function gtag(){dataLayer.push(arguments);}--}}
+{{--            gtag('js', new Date());--}}
 
-            gtag('config', 'UA-49160339-1');
-        </script>
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1254063,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
+{{--            gtag('config', 'UA-49160339-1');--}}
+{{--        </script>--}}
     </body>
 </html>
